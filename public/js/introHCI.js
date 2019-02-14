dff5e963a1ba9c1eb6d35630e438d56d522997e8
Joycelyn Peng <jjp040@ucsd.edu>
2019-02-14 02:43:58 -0800

    lab6 done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,16 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get(url, projectDetails);
+}
+
+function projectDetails(result) {
+	var projectHTML = '<p>' + result['title'] + '</p>' + '<p>' + result['date'] + '</p>' + '<img src="' + result['imamge'] + '" class="detailsImage">' + '<p>' + result['summary'] + '</p>';
+	//console.log(projectHTML);
+
+	var selector = "#project" + result['id'] + " .details";
+	//console.log(selector)
+
+	$(selector).html(projectHTML);
 }
